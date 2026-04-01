6839d1e497f855e928cf93e3a252d8b7659136d5
lucasmpg <moisslucas@hotmail.com>
2026-04-01 10:50:49 -0400

    Create runtime-config.js

---
+++ assets/js/runtime-config.js
@@ -0,0 +1,11 @@
+window.RUNTIME_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBwg5k0wXPZ4ZOeCfCjPHe95yDFJNSu_ww",
+    authDomain: "ark-fichas.firebaseapp.com",
+    projectId: "ark-fichas",
+    storageBucket: "ark-fichas.firebasestorage.app",
+    messagingSenderId: "1097313250279",
+    appId: "1:1097313250279:web:050f40ceca2ca90da36b42",
+    measurementId: "G-3HX09H36Q5"
+  }
+};
